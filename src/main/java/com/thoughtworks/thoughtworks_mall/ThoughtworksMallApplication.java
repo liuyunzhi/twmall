f54fd328306b35da496cb542264356a96feae7fa
Yunzhi Liu <liu_yun_zhi@163.com>
2018-08-01 21:36:11 +0800

    init project

---
+++ src/main/java/com/thoughtworks/thoughtworks_mall/ThoughtworksMallApplication.java
@@ -0,0 +1,12 @@
+package com.thoughtworks.thoughtworks_mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThoughtworksMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThoughtworksMallApplication.class, args);
+	}
+}
---
+++ src/test/java/com/thoughtworks/thoughtworks_mall/ThoughtworksMallApplicationTests.java
@@ -0,0 +1,16 @@
+package com.thoughtworks.thoughtworks_mall;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ThoughtworksMallApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
